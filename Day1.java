4c2320a9b8e441f26caa5300d2a92314748c2e32
chakyy569-byte <chakyy569@gmail.com>
2026-09-02 21:51:36 +0800

    Update Day1.java
    Perbaikan

--- Day1.java
+++ Day1.java
@@ -2,17 +2,17 @@
 * Documentacion Comment
 * Day1menuju100DaysofCoding
 */
- public class day01 {
+ public class Day1 {
    public static void main(String[] args) {
 
     // Single-Line Comment
-    System.out.printIn ("Hello Unsulbar") ;
+    System.out.println ("Hello Unsulbar") ;
 
     /*
       Multi-Line Comment
       Used to write long explanations or disable code blocks.
     */
 
-   System.out.printIn ("Hello Unsulbar") ;
+   System.out.println ("Hello Unsulbar") ;
    }
  }
