4aeff81a63a5d1ce3f5774433a447700bf0ce3fe
chakyy569-byte <chakyy569@gmail.com>
2026-09-04 14:07:56 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,25 @@
+/*
+ * Day3menuju100DayofCoding
+ */
+public class Day3 {
+    public static void main(String[] args) {
+        /*
+         * penggunaan (\n)
+         * fungsinya yaitu untuk membuat baris baru
+         */
+        System.out.println("Halo\nNama Saya Zaky");
+        /*
+         * penggunaan (\t)
+         * fungsinya yaitu untuk membuat jarak
+         */
+        System.out.println("umur :\t18");
+        System.out.println("hobi :\tmain game dan memasak");
+        /*
+         * penggunaan (\\)
+         * fungsinya yaitu untuk menampilkan backslash
+         */
+        System.out.println("kata-kata untuk hari ini : \"jangan lupa bahagia\"");
+	
+}
+    
+}
