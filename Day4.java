4010fa4f3a93a5916452cb350590f2145283e9be
chakyy569-byte <chakyy569@gmail.com>
2026-09-05 12:08:57 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,31 @@
+//Day4menuju100DaysofCoding
+public class Day4 {
+    public static void main(String[] args) {
+	
+        byte jumlahmahasiswainformatikakelasD = 49;
+        System.out.println("jumlah mahasiswa informatika kelas D    : " + jumlahmahasiswainformatikakelasD);
+        //byte memiliki ukuran 8-bit rentang dari -128 → 127
+        
+        short jumlahmahasiswabaruunsulbar = 3720;
+        System.out.println("jumlah mahasiswa baru unsulbar     : " + jumlahmahasiswabaruunsulbar);
+        //short memiliki ukuran 16-bit rentang dari -32.768 → 32.767
+        
+        int hargaNmax = 30000000;
+        int jumlah = 5;
+        System.out.println("Total       : " + (hargaNmax * jumlah));
+        int harga = 20000000;
+        System.out.println("harga iphone        : " + harga);
+        /*
+         * int adalah type data bilangan bulat yg paling sering di gunakan
+         * int memiliki ukuran ukuran 32-bit rentang dari -2.147.483.648 → 2.147.483.647
+         */
+         
+        long jarak = 149597870L;
+        System.out.println("jarak bumi ke matahari      : " + jarak);
+        /*
+         * long memiliki ukuran 64-bit rentang dari -9,22 kuintiliun sampai 9,22 kuintiliun
+         * khusus untuk long di berikan akhiran L
+         */
+}
+    
+}
