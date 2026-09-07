a1aa5ab7155b928644ebacca75c14735599ec972
chakyy569-byte <chakyy569@gmail.com>
2026-09-07 17:28:07 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+public class Day6 {
+    
+    public static void main (String[] args) {
+        String nama = "Chakyy";
+        int umur = 18;
+        double ipk = 3.9;
+        char kodejurusan = 'I';
+        boolean status = true;
+        
+        System.out.println("nama mahasiswa  :     " + nama);
+        System.out.println("umur            :       " + umur);
+        System.out.println("ipk             :        " + ipk);
+        System.out.println("kodejurusan     :       " + kodejurusan);
+        System.out.println("statusmahasiswa :       " + status);
+    }
+    
+}
