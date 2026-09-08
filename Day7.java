8504bee1f3176ae1c8c0b1df2889e092b68aea64
chakyy569-byte <chakyy569@gmail.com>
2026-09-08 16:40:44 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,18 @@
+//Day7menuju100DaysofCoding
+public class Day7 {
+    
+    public static void main(String[] args) {
+        String Nickname = "Chakyy";
+        int Level       = 120;
+        String Role     = "Assasin";
+        String Rank     = "Imortal";
+      
+        System.out.println("======= Profile =======");
+        System.out.println("Nickname   : " + Nickname);
+        System.out.println("Level      : " + Level);
+        System.out.println("Role       : " + Role);
+        System.out.println("Rank       : " + Rank);
+        System.out.println("=======================");
+    }
+    
+}
