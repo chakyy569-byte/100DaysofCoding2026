36b77be8ec9b8aff7db0523e624cd49baf1ed52f
chakyy569-byte <chakyy569@gmail.com>
2026-09-09 12:46:45 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,38 @@
+//Day8menuju100DaysofCoding
+public class Day8 {
+    
+    public static void main(String[] args) {
+        //deklarasi
+        String Nama_Player;
+        int level;
+        
+        //inisialisasi
+        Nama_Player     = "Chakyy";
+        level           = 50;
+        
+        //Data awal
+        System.out.println("╔════════════════════════════╗");
+        System.out.println("║       🎮 DATA AWAL 🎮           ║");
+        System.out.println("╠════════════════════════════╣");
+        System.out.println("  Nickname : " + Nama_Player);
+        System.out.println("  Level    : " + level);
+        System.out.println("╚════════════════════════════╝");
+        
+        //Update
+        Nama_Player     = "Chakyy Pro";
+        level           = 100;
+        
+        //DATA AKHIR
+        System.out.println("╔════════════════════════════╗");
+        System.out.println("║      🏆 DATA AKHIR 🏆           ║");
+        System.out.println("╠════════════════════════════╣");
+        System.out.println("  Nickname : " + Nama_Player);
+        System.out.println("  Level    : " + level);
+        System.out.println("  Status   : MAX LEVEL 🔥");
+        System.out.println("╚════════════════════════════╝");
+        
+        System.out.println("🔥 Chakyy berhasil naik level!");
+        System.out.println("🚀 LEVEL 100 TERCAPAI!");
+        
+    }
+          }
