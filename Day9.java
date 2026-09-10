ee6ce53cb145a7dc962cd5882d680f46d50fdc02
chakyy569-byte <chakyy569@gmail.com>
2026-09-10 18:26:50 +0800

    Add Day9 class with main method and output

---
+++ Day9.java
@@ -0,0 +1,16 @@
+// Day9Menuju100DaysOfCoding
+// Chakyy_26
+public class Day9 {
+    
+    public static void main(String[] args) {
+        final String nickname = " Chakyy26";
+        final int bintang = 126;
+        final String pernyataan = "season depan harus imo";
+        final String Info = pernyataan + " bintang " + bintang;
+        
+        System.out.println("nickname    : " + nickname);
+        System.out.println("bintang     : " + bintang);
+        System.out.println(Info);
+    }
+    
+}
