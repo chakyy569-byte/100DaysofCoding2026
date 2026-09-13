d3f8928520549298051e7420cb4983fb5ea3fc96
chakyy569-byte <chakyy569@gmail.com>
2026-09-13 19:15:00 +0800

    Add Day12.java for biodata input and display

---
+++ Day12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//Day12menuju100DaysofCoding
+public class Day12 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        String Nama = input.nextLine();
+        int Umur = input.nextInt();
+        double tinggibadan = input.nextDouble();
+        char jeniskelamin = input.next().charAt(0);
+        
+        System.out.println("\n=== BIODATA ===");
+        
+        System.out.printf("Nama\t\t: %s%n ", Nama);
+        System.out.printf("Umur\t\t: %d tahun%n", Umur);
+        System.out.printf("Tinggi\t\t:%.2f cm%n ", tinggibadan);
+        System.out.printf("Jenis Kelamin\t:%c%n ", jeniskelamin);
+        
+
+    }
+    
+}
