9d0dcd86f55fb8281b61234571223072aabbf75a
chakyy569-byte <chakyy569@gmail.com>
2026-09-15 17:08:01 +0800

    Add Day14 class with basic arithmetic operations

---
+++ Day14.java
@@ -0,0 +1,18 @@
+// Day14Menuju100DaysofCoding
+public class Day14 {
+    
+    public static void main(String[] args) {
+        
+        int A = 200;
+        int B = 100;
+        
+        int penjumlahan = A + B;
+        int pengurangan = A - B;
+        
+        System.out.println("Hasil penjumlahan   : " + penjumlahan);
+        System.out.println("Hasil pengurangan   : " + pengurangan);
+        
+        
+    }
+    
+}
