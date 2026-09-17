8d01f64e1c93666d3847623a457b62f19b76101c
chakyy569-byte <chakyy569@gmail.com>
2026-09-17 19:40:22 +0800

    Add Day16 class with modulus calculation

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class Day16 {
+    
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 4;
+        int hasil = A % B;
+      
+        System.out.println("Hasil modulus dari 10 % 4  : " + hasil);
+        
+        // Day16menuju100DaysOfCoding
+        
+    }
+    
+}
