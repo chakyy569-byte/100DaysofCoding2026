ea8a7072692c2f18f95ed70462915350af7e7853
chakyy569-byte <chakyy569@gmail.com>
2026-09-18 21:52:04 +0800

    Implement basic arithmetic operations in Day17.java
    Demonstrate basic arithmetic operations in Java.

---
+++ Day17.java
@@ -0,0 +1,31 @@
+public class Day17 {
+    
+    public static void main(String[] args) {
+        int A = 100;    // untuk memberi nilai
+        
+        int B = 100;
+        B += 50;        // Untuk menambah
+        
+        int C = 100;
+        C -= 50;        // Untuk mengurang
+        
+        int D = 100;
+        D *= 50;        // Untuk mengali
+        
+        int E = 100;
+        E /= 50;        // Untuk membagi
+        
+        int F = 100;
+        F %= 50;        // Untuk membagi sisa hasil pembagian
+        
+        
+        System.out.printf("A\t:%d%nB\t:%d%nC\t:%d%nD\t:%d%nE\t:%d%nF\t:%d", A, B, C, D, E, F);
+        
+        
+        //Day17Menuju100DayofCoding
+        // Halo kak hehe
+        
+        
+    }
+    
+}
