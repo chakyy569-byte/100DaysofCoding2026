ca96b18334622c0a6a58d7d1095c0afd86569732
chakyy569-byte <chakyy569@gmail.com>
2026-09-19 19:23:41 +0800

    Add type promotion example in Day18.java
    Demonstrate type promotion from byte to long.

---
+++ Day18.java
@@ -0,0 +1,23 @@
+// Day 18 Menunju 100 Days Of Coding
+public class Day18 {
+    
+    public static void main(String[] args) {
+        
+        
+        byte nilaibyte = 127;
+        
+        short nilaishort = nilaibyte;
+        
+        int nilaiint = nilaishort;
+        
+        long nilailong = nilaiint;
+        
+        
+        System.out.printf("Nilai byte\t\t:%d%nNilai Short\t\t:%d%nNilai int\t\t:%d%nNilai long\t\t:%d", nilaibyte, nilaishort, nilaiint, nilailong);
+
+// Chakyy
+       
+    
+    }
+   
+}
