3bf22f6f49a27bbc19a0033c3f3f52e36d64f2f8
chakyy569-byte <chakyy569@gmail.com>
2026-09-20 20:09:49 +0800

    Add Day19 class for type casting example
    Demonstrates type casting from double to byte.

---
+++ Day19.java
@@ -0,0 +1,20 @@
+// Day 19 menunju 100days of coding
+public class Day19 {
+    
+    public static void main(String[] args) {
+        
+        double nilaidouble = 7000.26;
+        
+        float nilaifloat = (float) nilaidouble;
+        
+        long nilailong = (long) nilaifloat;
+        
+        int nilaiint = (int) nilailong;
+        
+        short nilaishort = (short) nilaiint;
+        
+        byte nilaibyte = (byte) nilaishort;
+        System.out.printf("Nilai Double\t:%.2f%nNilai Float\t:%.2f%nNilai long\t:%d%nNilai int\t:%d%nNilai short\t:%d%nNilai byte\t:%d", nilaidouble, nilaifloat, nilailong, nilaiint, nilaishort, nilaibyte);
+    }
+    
+}
