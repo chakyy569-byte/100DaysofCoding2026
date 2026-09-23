6fe7740edbbed18b48436caf9ec648533b4f636f
chakyy569-byte <chakyy569@gmail.com>
2026-09-23 20:21:20 +0800

    Implement value swapping in Day22.java

---
+++ Day22.java
@@ -0,0 +1,27 @@
+public class Day22 {
+    
+    public static void main(String[] args) {
+        
+        int A = 222;
+        int B = 999;
+        
+        System.out.println("=== HASIL NYA SEBELEM DITUKAR ===");
+        System.out.println("\tNilai A       : " + A);
+        System.out.println("\tNilai B       : " + B);
+        
+        
+        int temp = A;
+        A = B;
+        B = temp;
+        
+        System.out.println("\n=== HASIL NYA SESUDAH DITUKAR ===");
+        System.out.println("\tNilai A       : " + A);
+        System.out.println("\tNilai B       : " + B);
+        
+        // #Day 22 Menuju 100 Days Of Coding
+        // yuhuuuuuuuuuuuu
+        // #Chakyy²⁶
+        // Info mabar
+    }
+    
+}
