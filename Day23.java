09c68beac1f71d596834057404ee1f9df92a0145
chakyy569-byte <chakyy569@gmail.com>
2026-09-24 21:18:23 +0800

    Implement square area calculation in Day23.java
    This program calculates the area of a square with a side length of 10 cm and displays the result.

---
+++ Day23.java
@@ -0,0 +1,30 @@
+public class Day23 {
+    
+    public static void main(String[] args) {
+        /*SOAL
+         * Buatlah program Java untuk menghitung luas
+         * sebuah persegi. Program memiliki panjang sisi
+         * sebesar 10 cm. Gunakan rumus luas persegi dan
+         * tampilkan hasilnya ke layar.
+         */
+        
+        // DIKETAHUI :
+        int sisi = 10;
+        
+        // penyelesaian 
+        int luas = sisi * sisi;
+        
+        //output nya
+        System.out.println("=== Hasil ===");
+        System.out.println("Luas        : " + luas + " cm");
+        
+        // jadi luas persegi tersebut itu adalah 100cm
+        
+        // #Day 23 menuju 100 Days Of Coding
+        // Chakyy 
+        // Hahahahha
+        
+        
+    }
+    
+}
