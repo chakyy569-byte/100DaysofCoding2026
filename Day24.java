39894d9ccedbaef7941540bea4830e5f4128ac92
chakyy569-byte <chakyy569@gmail.com>
2026-09-25 08:31:19 +0800

    Add rectangle area calculation in Day24.java
    Implement a program to calculate the area of a rectangle.

---
+++ Day24.java
@@ -0,0 +1,27 @@
+public class Day24 {
+    
+    public static void main(String[] args) {
+         
+        /*Soal :
+         * Sebuah persegi panjang memiliki
+         * panjang 30cm dan lebar 10cm
+         * hitunglah berapa luasnya
+         */
+        
+        // input
+        int panjang = 30;
+        int lebar   = 10;
+        
+        // process
+        int luas = panjang * lebar;
+        // output
+        System.out.println("=== HASIL ===");
+        System.out.println("Luas    : " + luas + " cm");
+        
+        
+        // Day 24 Menuju 100 Days Of Coding 
+        // Chakyy²⁶
+        // Cihuyyy
+    }
+    
+}
